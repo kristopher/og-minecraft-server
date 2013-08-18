c2815fc3a32a4ac90b5173b5b7f8719d8a1d00cb
Kristopher Chambers <kristopher.chambers@gmail.com>
2013-08-18 11:17:22 +0000

    Initial

---
+++ mc-overviewer/config.py
@@ -0,0 +1,8 @@
+worlds["OG"] = "/var/minecraft/server/world"
+
+renders["normalrender"] = {
+    "world": "OG",
+    "title": "Da Shiz",
+}
+
+outputdir = "/var/minecraft/mc-overviewer/render"
---
+++ server/ServerStart.sh
@@ -0,0 +1,1 @@
+java -Xms512M -Xmx1G -XX:PermSize=128m -jar ftbserver.jar
